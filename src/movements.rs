240f12f43b8617c622de3cc74d115c38bc1e0ba3
FrancoGiachetta27 <fgiachetta@frba.utn.edu.ar>
2023-02-25 13:10:50 -0300

    added correct pawn killing

--- src/movements.rs
+++ src/movements.rs
@@ -10,7 +10,7 @@ use bevy_ecs_tilemap::{
 
 use crate::{
     board::{Tile, TileState},
-    piece::{spawn_circle, Team},
+    piece::{highlight_tile, Team},
 };
 
 pub fn pawn_movement(
@@ -19,37 +19,55 @@ pub fn pawn_movement(
     tile_pos: TilePos,
     tile_storage: &TileStorage,
     tile_state_q: &mut Query<&mut TileState>,
+    piece_team: &Query<&Team>,
+    selection_ent: Entity,
     grid_size: &TilemapGridSize,
     map_size: &TilemapSize,
     map_type: &TilemapType,
-    piece_team: &Query<&Team>,
     meshes: &mut Assets<Mesh>,
     materials: &mut Assets<ColorMaterial>,
 ) {
-    let neighbor_position = Neighbors::get_square_neighboring_positions(&tile_pos, map_size, true);
-
-    let north_neighbor: &TilePos;
+    let square_neighbors = Neighbors::get_square_neighboring_positions(&tile_pos, map_size, true);
+    let neighbor_directions: [SquareDirection; 3];
 
     if let Team::White = piece_team.get(selection).unwrap() {
-        north_neighbor = neighbor_position.get(SquareDirection::North).unwrap();
+        neighbor_directions = [
+            SquareDirection::NorthWest,
+            SquareDirection::North,
+            SquareDirection::NorthEast,
+        ];
     } else {
-        north_neighbor = neighbor_position.get(SquareDirection::South).unwrap();
+        neighbor_directions = [
+            SquareDirection::SouthWest,
+            SquareDirection::South,
+            SquareDirection::SouthEast,
+        ];
     }
 
-    let tile_ent = tile_storage.get(&north_neighbor).unwrap();
-    let mut tile_s = tile_state_q.get_mut(tile_ent).unwrap();
-
-    if let Tile::Empty = tile_s.tile_type {
-        tile_s.tile_type = Tile::WithCircle;
-        spawn_circle(
-            commands,
-            grid_size,
-            map_type,
-            north_neighbor,
-            meshes,
-            materials,
-        );
-    }
+    neighbor_directions.into_iter().for_each(|dir| {
+        if let Some(neighbor) = square_neighbors.get(dir) {
+            let tile_ent = tile_storage.get(&neighbor).unwrap();
+            let mut tile_s = tile_state_q.get_mut(tile_ent).unwrap();
+            match dir {
+                SquareDirection::North | SquareDirection::South => {
+                    if let Tile::Empty = tile_s.tile_type {
+                        tile_s.tile_type = Tile::HighLighted;
+                        highlight_tile(commands, grid_size, map_type, neighbor, meshes, materials);
+                    }
+                }
+                _ => {
+                    if let Some(e) = tile_s.piece_ent {
+                        if piece_team.get(e).unwrap() != piece_team.get(selection_ent).unwrap() {
+                            tile_s.tile_type = Tile::HighLighted;
+                            highlight_tile(
+                                commands, grid_size, map_type, neighbor, meshes, materials,
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    })
 }
 
 pub fn king_movement(
@@ -57,6 +75,8 @@ pub fn king_movement(
     tile_storage: &TileStorage,
     tile_pos: TilePos,
     tile_state_q: &mut Query<&mut TileState>,
+    piece_team: &Query<&Team>,
+    selection_ent: Entity,
     grid_size: &TilemapGridSize,
     map_size: &TilemapSize,
     map_type: &TilemapType,
@@ -74,8 +94,13 @@ pub fn king_movement(
 
         //check wether there is a piece on the tile
         if let Tile::Empty = tile_s.tile_type {
-            tile_s.tile_type = Tile::WithCircle;
-            spawn_circle(commands, grid_size, map_type, pos, meshes, materials);
+            tile_s.tile_type = Tile::HighLighted;
+            highlight_tile(commands, grid_size, map_type, pos, meshes, materials);
+        } else if let Some(e) = tile_s.piece_ent {
+            if piece_team.get(e).unwrap() != piece_team.get(selection_ent).unwrap() {
+                tile_s.tile_type = Tile::HighLighted;
+                highlight_tile(commands, grid_size, map_type, pos, meshes, materials);
+            }
         }
     }
 }
@@ -85,12 +110,14 @@ pub fn knight_movement(
     tile_storage: &TileStorage,
     tile_pos: TilePos,
     tile_state_q: &mut Query<&mut TileState>,
+    piece_team: &Query<&Team>,
+    selection_ent: Entity,
     grid_size: &TilemapGridSize,
     map_type: &TilemapType,
     meshes: &mut Assets<Mesh>,
     materials: &mut Assets<ColorMaterial>,
 ) {
-    let directions: Vec<(i32, i32)> = vec![
+    let directions: [(i32, i32); 8] = [
         (1, 2),
         (-1, 2),
         (1, -2),
@@ -120,8 +147,13 @@ pub fn knight_movement(
 
                 //check wether there is a piece on the tile
                 if let Tile::Empty = tile_s.tile_type {
-                    tile_s.tile_type = Tile::WithCircle;
-                    spawn_circle(commands, grid_size, map_type, &new_pos, meshes, materials);
+                    tile_s.tile_type = Tile::HighLighted;
+                    highlight_tile(commands, grid_size, map_type, &new_pos, meshes, materials);
+                } else if let Some(e) = tile_s.piece_ent {
+                    if piece_team.get(e).unwrap() != piece_team.get(selection_ent).unwrap() {
+                        tile_s.tile_type = Tile::HighLighted;
+                        highlight_tile(commands, grid_size, map_type, &new_pos, meshes, materials);
+                    }
                 }
             }
         }
@@ -135,6 +167,8 @@ pub fn sequencial_pieces(
     map_size: &TilemapSize,
     map_type: &TilemapType,
     tile_state_q: &mut Query<&mut TileState>,
+    piece_team: &Query<&Team>,
+    selection_ent: Entity,
     pos: TilePos,
     mesh: &mut Assets<Mesh>,
     material: &mut Assets<ColorMaterial>,
@@ -142,35 +176,54 @@ pub fn sequencial_pieces(
 ) {
     let tile_neighbors = Neighbors::get_square_neighboring_positions(&pos, map_size, true);
 
-    //spawn in every specified direction
+    // spawn in every specified direction
     neighbor_directions.iter().for_each(|dir| {
         if let Some(pos) = tile_neighbors.get(*dir) {
-            let mut new_pos = *pos;
+            let mut current_pos = *pos;
             let mut tile_ent = tile_storage.get(&pos).unwrap();
             //tile state
             let mut tile_s = tile_state_q.get_mut(tile_ent).unwrap();
 
-            //check wether there is a piece on the tile
-            if let Tile::Empty = tile_s.tile_type {
-                tile_s.tile_type = Tile::WithCircle;
-                spawn_circle(commands, grid_size, map_type, &pos, mesh, material);
+            // checks if the first the closest tile is empty or else
+            // if it has a piece with opposite color of the selection
+            if matches!(tile_s.tile_type, Tile::Empty) {
+                tile_s.tile_type = Tile::HighLighted;
+                highlight_tile(commands, grid_size, map_type, &pos, mesh, material);
 
-                //gets the neighbor which is in the direction specified, and spawns the circle, it
-                //keeps doing it until there's a piece or it reaches the end
-                while let Some(n) =
-                    Neighbors::get_square_neighboring_positions(&new_pos, map_size, true).get(*dir)
+                // gets the neighbor which is in the direction specified, and spawns the circle, it
+                // keeps doing it until there's a piece or it reaches the end
+                while let Some(n_pos) =
+                    Neighbors::get_square_neighboring_positions(&current_pos, map_size, true)
+                        .get(*dir)
                 {
-                    tile_ent = tile_storage.get(&n).unwrap();
+                    tile_ent = tile_storage.get(&n_pos).unwrap();
                     tile_s = tile_state_q.get_mut(tile_ent).unwrap();
 
-                    // changes the position to be the last accessed neighbor's
-                    new_pos = TilePos { x: n.x, y: n.y };
+                    // changes the position to be the neighbor's last accessed
+                    current_pos = TilePos {
+                        x: n_pos.x,
+                        y: n_pos.y,
+                    };
+
                     if let Tile::Empty = tile_s.tile_type {
-                        tile_s.tile_type = Tile::WithCircle;
-                        spawn_circle(commands, grid_size, map_type, &n, mesh, material);
-                    } else {
+                        tile_s.tile_type = Tile::HighLighted;
+                        highlight_tile(commands, grid_size, map_type, &n_pos, mesh, material);
+                    } else if let Some(e) = tile_s.piece_ent {
+                        // checks if it's color is the opposite of the selection's
+                        if piece_team.get(e).unwrap() != piece_team.get(selection_ent).unwrap() {
+                            tile_s.tile_type = Tile::HighLighted;
+                            highlight_tile(commands, grid_size, map_type, &n_pos, mesh, material);
+                        }
+
                         break;
                     }
+                }
+            } else {
+                if piece_team.get(tile_s.piece_ent.unwrap()).unwrap()
+                    != piece_team.get(selection_ent).unwrap()
+                {
+                    tile_s.tile_type = Tile::HighLighted;
+                    highlight_tile(commands, grid_size, map_type, &pos, mesh, material);
                 }
             }
         }
